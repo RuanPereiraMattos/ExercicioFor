dd8a2195a0999f2bf4efbe738fd1ac8d08fcd00b
Ruan Pereira Mattos <ruandupg@gmail.com>
2024-03-11 20:04:26 -0300

    Primeira versão

---
+++ src/main/java/com/ruanmattos/main/ExercicioFor.java
@@ -0,0 +1,13 @@
+package com.ruanmattos.main;
+
+/**
+ * Exercício sobre for
+ *
+ * @author Ruan Pereira Mattos
+ */
+public class ExercicioFor {
+
+    public static void main(String[] args) {
+
+    }
+}
