406f5f430213fc48935e97aab276d5372ab3e6ab
Ruan Pereira Mattos <ruandupg@gmail.com>
2024-03-11 21:59:27 -0300

    Melhoras e implementações com o que foi visto em sala de aula

--- src/main/java/com/ruanmattos/main/ExercicioFor.java
+++ src/main/java/com/ruanmattos/main/ExercicioFor.java
@@ -1,5 +1,7 @@
 package com.ruanmattos.main;
 
+import java.util.Scanner;
+
 /**
  * Exercício sobre for
  *
@@ -8,6 +10,21 @@ package com.ruanmattos.main;
 public class ExercicioFor {
 
     public static void main(String[] args) {
+
+        Scanner s = new Scanner(System.in);
+        System.out.print("Digite quantos números quer somar: ");
+        int n = s.nextInt();
+
+        int soma = 0;
+        System.out.println("Quais números gostaria de somar?");
+        for (int i = 0; i < n; i++) {
+            System.out.print("Digite o número " + (i + 1) + " : ");
+            int x = s.nextInt();
+            soma += x;
+        }
+
+        System.out.println("A soma de todos os números digitados é: " + soma);
+        s.close();
 
     }
 }
